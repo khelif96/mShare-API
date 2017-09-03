72112ad94587c4e4465d91b3ac6f73033662e673
Mohamed Khelif <khelif96@gmail.com>
2017-09-03 16:24:11 -0400

    created new schema imageNote to allow

---
+++ app/models/imageNote.js
@@ -0,0 +1,16 @@
+var mongoose  = require('mongoose');
+var Schema = mongoose.Schema;
+
+// Image Note Schema
+var imageNoteSchema = new Schema({
+  Title: String,
+  Content: String,
+  imageUrl: String,
+  author_id: String,
+  NoteType: {type: String, default: "image"},
+  is_public: {type: Boolean, default: false},
+  access_ids: [],
+  createdDate: {type: Date, default:Date.now}
+});
+
+module.exports = mongoose.model('ImageNote', imageNoteSchema);
