a0e5646f7221fe1669696fc3121acf0c03236532
Mohamed Khelif <khelif96@gmail.com>
2017-09-03 16:22:23 -0400

    added new noteType field in note schema

--- app/models/note.js
+++ app/models/note.js
@@ -6,6 +6,7 @@ var noteSchema = new Schema({
   Title: String,
   Content: String,
   author_id: String,
+  noteType: {type:String,default:"text"},
   is_public: {type: Boolean, default: false},
   access_ids: [],
   createdDate: {type: Date, default:Date.now}
